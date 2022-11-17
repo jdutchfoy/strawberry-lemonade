e4f3be8d7e407b7fd169a9ba46accb0c2c525261
Dutch Foy <jdutchfoy@gmail.com>
2022-11-17 08:49:11 -0800

    java alerts

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+let allergies =prompt("Any allergies")
+alert(allergies)
+alert (allergies)
+allergies
+let allergies2 =prompt("Any allergies")
+alert(allergies2)
+let thirst =prompt("Are you thirsty")
+alert(thirst)
