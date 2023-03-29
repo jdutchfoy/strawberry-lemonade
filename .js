e4d8fd6f2e1de8ab3988c9a62bc5597a23f9be23
dutch foy <jdutchfoy@gmail.com>
2023-03-29 12:15:52 -0700

    html overhaul I dunno maybe

---
+++ .js
@@ -0,0 +1,5 @@
+const recipeButton = document.querySelector("#recipe-button");
+
+recipeButton.addEventListener("click", () => {
+  window.alert("Coming Soon: Dutch's Famous Strawberry Lemonade Recipes!");
+});
